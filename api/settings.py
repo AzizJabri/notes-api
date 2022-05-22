2c64882885be76309a89f72742fe17b90ede195f
Aziz Jabri <79476299+AzizJabri@users.noreply.github.com>
2022-05-22 16:43:11 +0100

    updated cors

--- api/settings.py
+++ api/settings.py
@@ -58,8 +58,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "PATCH",
+    "POST",
 ]
 
 ROOT_URLCONF = 'api.urls'
